7fa3a060e1183719a5b0c86b344d118be21eb436
Williano <paawilly17@gmail.com>
2019-02-15 00:46:36 +0000

    [NEW CODE] :white_check_mark: Add views and controllers

---
+++ Restaurant/MenuItemDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MenuItemDetailViewController.swift
+//  Restaurant
+//
+//  Created by William Kpabitey Kwabla on 2/14/19.
+//  Copyright © 2019 William Kpabitey Kwabla. All rights reserved.
+//
+
+import UIKit
+
+class MenuItemDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by William Kpabitey Kwabla on 2/14/19.
+//  Copyright © 2019 William Kpabitey Kwabla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
